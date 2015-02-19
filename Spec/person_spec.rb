d81f3a61cfe4d1e95264d8139c62dcebc3f9948c
njewootah <njewootah@spartaglobal.com>
2015-02-19 10:14:36 +0000

    Added test cases including exercise 2

--- Spec/person_spec.rb
+++ Spec/person_spec.rb
@@ -1,44 +1,110 @@
 require 'spec_helper'
 
 describe 'Person' do
+
+  let(:dob){'1990-01-01'}
+  let(:fname){'Joe'}
+  let(:sname){'Bloggs'}
+  let(:fullname){'Joe Bloggs'}
+  let(:email){'njewootah@yahoo.com'}
+  let(:phone){'02086414175'}
+  let(:person){Person.new(fname,sname,dob)}
+
   describe 'The person class is an addressbook entry' do
     it "should store and return personal information" do
-      person = Person.new('Joe','Bloggs', '1 Jan 1990')
-
-      #Expected outcomes
       expect(person.dob).to be_a(Date)
-      expect(person.dob.to_s).to eq('1990-01-01')
-      expect(person.fname).to eq('Joe')
-      expect(person.sname).to eq('Bloggs')
-      expect(person.fullname).to eq('Joe Bloggs')
+      expect(person.dob.to_s).to eq(dob)
+      expect(person.fname).to eq(fname)
+      expect(person.sname).to eq(sname)
+      expect(person.fullname).to eq(fullname)
     end
 
     it "should accept optional dates" do
-      person = Person.new('Joe','Bloggs')
-
-      #Expected outcomes
+      person = Person.new(fname,sname)
       expect(person.dob).to be_nil
     end
 
-    it "should allow emails to be added to personal information" do
-      person = Person.new('Joe','Bloggs')
-      person.add_email('njewootah@yahoo.com')
+    #it "should raise an error if full name is not provided" do
+      #expect(Person.new(fname)).to raise_error(ArgumentError)
+      #expect(Person.new('',sname)).to raise_error(ArgumentError)
+    #end
 
-      #Expected outcomes
-      expect(person.emails).to be_a(Array)
-      expect(person.emails[0]).to eq('njewootah@yahoo.com')
+    it "should allow emails to be added to personal information" do
+      expect(person.emails).to eq(Array.new)
+      person.add_email(email)
+      expect(person.emails[0]).to eq(email)
       expect(person.emails[1]).to be_nil
     end
 
-    it "should allow phone numbers to be added to personal information" do
-      person = Person.new('Joe','Bloggs')
-      person.add_phone_numbers('02086414175')
+    it "should allow emails to be removed from personal information" do
+      person.add_email(email)
+      person.remove_email(email)
+      expect(person.emails).to eq(Array.new)
+    end
 
-      #Expected outcomes
-      expect(person.phone_numbers).to be_a(Array)
-      expect(person.phone_numbers[0]).to eq('02086414175')
+    it "should allow phone numbers to be added to personal information" do
+      expect(person.phone_numbers).to eq(Array.new)
+      person.add_phone_numbers(phone)
+      expect(person.phone_numbers[0]).to eq(phone)
       expect(person.phone_numbers[1]).to be_nil
+    end
+
+    it "should allow phone numbers to be removed from personal information" do
+      person.add_phone_numbers(phone)
+      person.remove_phone_numbers(phone)
+      expect(person.phone_numbers).to eq(Array.new)
     end
   end
 end
 
+describe 'FamilyMember' do
+  let(:rel){'Father'}
+  let(:dob){'1990-01-01'}
+  let(:fname){'Joe'}
+  let(:sname){'Bloggs'}
+  let(:family){FamilyMember.new(fname,sname,dob)}
+
+  describe 'The FamilyMember class is a child of the Person class' do
+    it "should store the relationship status between Person objects" do
+      family.relationship = rel
+      expect(family.relationship).to eq(rel)
+      expect(family.fname).to eq(fname)
+      expect(family.sname).to eq(sname)
+      expect(family.dob.to_s).to eq(dob)
+      expect(FamilyMember < Person).to be true
+    end
+  end
+end
+
+describe 'AddressBook' do
+  let(:dob){'1990-01-01'}
+  let(:fname){'Joe'}
+  let(:sname){'Bloggs'}
+  let(:person){Person.new(fname,sname,dob)}
+  let(:person2){Person.new(fname,sname,dob)}
+  let(:fullname){fname + ' ' + sname}
+
+  describe 'The AddressBook class contains all contact information for all Person objects' do
+    it "should allow entry of contacts" do
+      book = AddressBook.new
+      expect(book.contacts).to eq(Array.new)
+      book.add(person)
+      expect(person.class).to eq(Person)
+      expect(book.contacts[0]).to eq(person)
+    end
+
+    #it "should display all entries of contacts" do
+      #book = AddressBook.new
+      #book.add(person)
+      #book.add(person2)
+      #expect(book.list[0]).to eq(person.fullname)
+      #expect(book.list[1]).to eq(person2.fullname)
+    #end
+
+    it "should allow the removal of contacts" do
+      book = AddressBook.new
+      book.remove('Joe')
+      expect(book.contacts).not_to include('Joe Bloggs')
+    end
+  end
+end
--- tddexercise.rb
+++ tddexercise.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'pry'
 
 class Person
   attr_accessor :dob,:fname,:sname
@@ -10,6 +11,7 @@ class Person
     else
       @dob = nil
     end
+
     @fname = fname
     @sname = sname
     @emails = []
@@ -24,7 +26,67 @@ class Person
     @emails << index
   end
 
+  def remove_email(index)
+    @emails.delete(index)
+  end
+
   def add_phone_numbers(index)
     @phone_numbers << index
   end
+
+  def remove_phone_numbers(index)
+    @phone_numbers.delete(index)
+  end
+
+  def to_s()
+    if @dob != nil
+      puts "#{@fullname} was born on #{@dob}.His email addresses are 
+      #{@emails}. His phone numbers are #{@phone_numbers}"
+    else
+      puts "The email addresses of #{@fullname} are #{@emails}. 
+      His phone numbers are #{@phone_numbers}"
+    end  
+  end
 end 
+
+class FamilyMember < Person
+  attr_accessor :relationship
+
+  def initialize(relationship = "relative",*args)
+    @relationship = relationship
+    super
+  end
+end
+
+class AddressBook
+  attr_accessor :contacts
+
+  def initialize()
+    @contacts = []
+  end
+
+  def add(person)
+    if person.is_a?Person
+      @contacts << person
+    else
+      puts "Please use an object of Person"
+    end
+  end
+
+  def list()
+    puts "\n\nAddress Book:"
+    puts "-------------"
+    @contacts.each_with_index {|item, index|
+    puts "Entry #{index + 1}: #{item.fullname}"}
+  end
+
+  def remove(fname)
+    @contacts.each do |i|
+      @contacts.each do |val|
+        if val == fname
+          @contacts.delete(i)
+        end
+      end
+    end
+  end
+end
